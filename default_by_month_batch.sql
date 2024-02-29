2060e5fadd9f18f39783ceed5583eb04e3ab4d7f
lorenzosc <lorenzocorrea6@hotmail.com>
2024-02-28 23:46:25 -0300

    Include presentation

--- default_by_month_batch.sql
+++ default_by_month_batch.sql
@@ -7,8 +7,8 @@ WITH all_entries AS (
 	FROM loans
 	LEFT JOIN clients ON loans.user_id = clients.user_id
 	GROUP BY clients.batch, month_year
-	ORDER BY month_year, clients.batch
+	ORDER BY clients.batch, month_year
 )
 SELECT month_year, batch, default_rate, loans
 FROM all_entries
-WHERE DATE '2024-01-25' - month_year > 80; -- WHERE current_date - month_year > 80 for a real case
+WHERE DATE '2024-01-25' - month_year > 90; -- WHERE current_date - month_year > 80 for a real case
